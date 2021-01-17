d3a3812fa3426249b509cf20fafa30d6f09a1751
nathaclmapaulino <nathapaulino17@gmail.com>
2021-01-17 15:06:34 +0000

    [Scripts] Creating the first version of CI/CD script!

---
+++ infra/scripts/ci-cd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# Este script é responsável por fazer toda a lógica do CI e até mesmo CD
+# das aplicações que compõem esse sistema!
+
+function deploy_environment() {
+  return 0
+}
+
+function test_service() {
+	return 0
+}
+
+function deploy_service() {
+	return 0
+}         
+
+function deploy_all() {
+	return 0
+}
+
+function push_docker_image_service() {
+	return 0
+}
+# Script se inicia aqui!
+
